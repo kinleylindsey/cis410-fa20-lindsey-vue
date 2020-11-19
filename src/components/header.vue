aafbf8d597e9623be49a785f9971783064987875
kinleylindsey <kinley@rams.colostate.edu>
2020-11-19 15:13:46 -0700

    13.2 11/19 axios, signup

--- src/components/header.vue
+++ src/components/header.vue
@@ -8,7 +8,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <router-link class="nav-link" to="/camera">Link</router-link>
+        <router-link class="nav-link" to="/camera">Camera</router-link>
       </li>
        <li class="nav-item">
         <router-link class="nav-link" to="/account">My Account</router-link>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './routes.js'
 
+axios.defaults.baseURL = " ";
 new Vue({
   el: '#app',
   router,
