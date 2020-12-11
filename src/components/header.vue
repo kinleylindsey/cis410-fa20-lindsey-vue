279a68ccbf56057a595f68683d2caaae825242c8
kinleylindsey <kinley@rams.colostate.edu>
2020-12-11 12:49:43 -0700

    12.11 project login

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,7 @@
 <template>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <router-link class="navbar-brand" to='/'>Navbar</router-link>
+  <router-link class="navbar-brand" to='/'>Lindsey</router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
+import store from './store.js'
 import router from './routes.js'
 
-axios.defaults.baseURL = "";
+axios.defaults.baseURL = "https://cis410-fa20-lindsey-api.azurewebsites.net/";
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
